42965d1c303021e7be3de4e0ecebe644dc19e426
Haruya1030 <sunny10spring@outlook.jp>
2026-08-29 19:48:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,73 @@
-const CACHE_NAME = "medal-game-v1";
+const CACHE_NAME = "medal-game-v2";
 
 const FILES = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
+/* インストール */
+
 self.addEventListener("install", event => {
+
   event.waitUntil(
+
     caches.open(CACHE_NAME).then(cache => {
+
       return cache.addAll(FILES);
+
     })
+
   );
+
+  self.skipWaiting();
+
 });
 
+
+/* 有効化 */
+
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      )
-    )
+
+      );
+
+    })
+
   );
+
+  self.clients.claim();
+
 });
 
+
+/* 通信 */
+
 self.addEventListener("fetch", event => {
+
   event.respondWith(
+
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+
+      if(response){
+        return response;
+      }
+
+      return fetch(event.request);
+
     })
+
   );
+
 });
